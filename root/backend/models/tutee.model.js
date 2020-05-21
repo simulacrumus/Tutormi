cdb6792cf6c63ca2730df6b6ea30c0b8110732ea
franic220 <fran0368@algonquinlive.com>
2020-05-21 12:12:50 -0400

    Dynamically renders tutor profiles, connects each tutor to their id, removes right panel from main

--- root/backend/models/tutee.model.js
+++ root/backend/models/tutee.model.js
@@ -10,8 +10,8 @@ const TuteeSchema = new mongoose.Schema({
     max: 300,
   },
   location: {
-      type: String,
-      max: 25
+    type: String,
+    max: 25,
   },
   appointments: [
     {
@@ -33,74 +33,83 @@ const TuteeSchema = new mongoose.Schema({
       },
       rate: {
         type: mongoose.Schema.Types.Decimal128,
-      }
-    }
+      },
+    },
   ],
   followingTutors: [
     {
       tutor: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "tutor",
-      }
-    }
+      },
+    },
   ],
   blockedTutors: [
     {
       tutor: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "tutor",
-      }
-    }
+      },
+    },
   ],
-  
-    appointments: [{
-        appointment: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'appointment'
-        }
-    }],
-    languages: [{
-        language: {
-            type: String,
-            max: 20
-        }
-    }],
-    ratings: [{
-        tutor: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'tutor'
-        },
-        rate: {
-            type: Number
-        }
-    }],
-    followingTutors: [{
-        tutor: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'tutor'
-        }
-    }],
-    blockedTutors: [{
-        tutor: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'tutor'
-        }
-    }],
-    social: {
-        linkedin: {
-            type: String
-        },
-        twitter: {
-            type: String
-        },
-        facebook: {
-            type: String
-        },
-        instagram: {
-            type: String
-        }
-    }
-  }
+
+  appointments: [
+    {
+      appointment: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "appointment",
+      },
+    },
+  ],
+  languages: [
+    {
+      language: {
+        type: String,
+        max: 20,
+      },
+    },
+  ],
+  ratings: [
+    {
+      tutor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "tutor",
+      },
+      rate: {
+        type: Number,
+      },
+    },
+  ],
+  followingTutors: [
+    {
+      tutor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "tutor",
+      },
+    },
+  ],
+  blockedTutors: [
+    {
+      tutor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "tutor",
+      },
+    },
+  ],
+  social: {
+    linkedin: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
 });
 
 module.exports = Tutee = mongoose.model("tutee", TuteeSchema);
