7fec61eb213fddb96601247282d976929dd21561
franic220 <fran0368@algonquinlive.com>
2020-05-27 17:00:49 -0400

    Adds date functionality to search

--- root/frontend/src/Router.js
+++ root/frontend/src/Router.js
@@ -8,47 +8,56 @@ import SearchMain from "./components/SearchMain";
 import TutorDashboardPage from "./pages/TutorDashboardPage.js";
 import { connect } from "react-redux";
 import Login from "./components/login/Login";
-import SignUp from "./components/login/SignUp"
+import SignUp from "./components/login/SignUp";
 
 class Router extends Component {
-
   isTutee = () => this.props.user.user.type === "tutee";
-  isAuthenticated = () => this.props.token !== null && this.props.token !== undefined;
+  isAuthenticated = () =>
+    this.props.token !== null && this.props.token !== undefined;
 
   render() {
     return (
       <main>
         <Switch>
-          <Route path="/" component={Login} exact />
-
+          <Route path="/" component={SearchMain} exact />
           <Route path="/login" component={Login} exact />
-
-          <Route path="/profile" exact
+          <Route
+            path="/profile"
+            exact
             render={() => {
-              return this.isAuthenticated() ? <ProfilePage /> : <Redirect to="/login" />;
-            }} />
-
-          <Route path="/dashboard" exact
+              return this.isAuthenticated() ? (
+                <ProfilePage />
+              ) : (
+                <Redirect to="/login" />
+              );
+            }}
+          />
+          <Route
+            path="/dashboard"
+            exact
             render={() => {
-              if (!this.isAuthenticated())
-                return <Redirect to="/login" />;
+              if (!this.isAuthenticated()) return <Redirect to="/login" />;
               else
-                return this.isTutee() ? <DashboardPage /> : <TutorDashboardPage />;
-
-            }} />
-
+                return this.isTutee() ? (
+                  <DashboardPage />
+                ) : (
+                  <TutorDashboardPage />
+                );
+            }}
+          />
           <Route path="/viewTutor" exact component={TutorViewPage} />
-
           <Route
             path="/search"
             render={() => {
-              if (!this.isAuthenticated())
-                return <Redirect to="/login" />;
+              if (!this.isAuthenticated()) return <Redirect to="/login" />;
               else
-                return this.isTutee() ? <SearchMain /> : <Redirect to="/profile" />;
-
-            }} />
-
+                return this.isTutee() ? (
+                  <SearchMain />
+                ) : (
+                  <Redirect to="/profile" />
+                );
+            }}
+          />
           <Route path="*" render={() => <h1>404 Page Not Found!</h1>} />{" "}
           {/* Need to make an error component later */}
         </Switch>
@@ -60,7 +69,7 @@ class Router extends Component {
 function mapStateToProps(state) {
   return {
     user: state.userReducer.user,
-    token: state.userReducer.token
+    token: state.userReducer.token,
   };
 }
 
